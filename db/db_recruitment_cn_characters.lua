656b16a4077a5aa71556f47c60e0e19f2075da91
Jah Raphael <jahraphael@yahoo.com>
2026-07-04 09:42:15 +0000

    [Raider.IO] Database Refresh

--- db/db_recruitment_cn_characters.lua
+++ db/db_recruitment_cn_characters.lua
@@ -2,10 +2,15 @@
 -- Copyright (c) 2026 by RaiderIO, Inc.
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="cn",date="2026-07-03T22:25:47Z",numCharacters=31034,db={}}
+local provider={name=...,data=3,region="cn",date="2026-07-04T09:28:46Z",numCharacters=30977,db={}}
 local F
 
-F = function() provider.db["熊猫酒仙"]={0,"不被世界催眠","浅丨喜"} end F()
+F = function() provider.db["熊猫酒仙"]={0,"不被世界催眠","墨林","浅丨喜"} end F()
+F = function() provider.db["死亡之翼"]={6,"Krazykarldk","Yili","小鹌鹑依德","海德森","王健身","电吉他音","芷兰寺","马剑克甲","骗骗圣光","龙之激动"} end F()
+F = function() provider.db["凤凰之神"]={26,"上帝的丶步兵"} end F()
+F = function() provider.db["闪电之刃"]={28,"邪魅一笑"} end F()
+F = function() provider.db["罗宁"]={30,"关耳"} end F()
+F = function() provider.db["主宰之剑"]={32,"战地术士"} end F()
 
 F = nil
 RaiderIO.AddProvider(provider)
--- db/db_recruitment_cn_lookup.lua
+++ db/db_recruitment_cn_lookup.lua
@@ -2,11 +2,11 @@
 -- Copyright (c) 2026 by RaiderIO, Inc.
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="cn",date="2026-07-03T22:25:47Z",numCharacters=31034,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
+local provider={name=...,data=3,region="cn",date="2026-07-04T09:28:46Z",numCharacters=30977,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
 local F
 
--- chunk size: 4
-F = function() provider.lookup[1] = "?\13?\13" end F()
+-- chunk size: 34
+F = function() provider.lookup[1] = "?\13?\29?\13?\29?\29?\29?\29?\29?\29?\29?\29?\29?\29?\29?\29?\29?\29" end F()
 
 F = nil
 RaiderIO.AddProvider(provider)
