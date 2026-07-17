bd02f5fd92a6a6d47c5a1b8162a6999d34b588c8
Jah Raphael <jahraphael@yahoo.com>
2026-07-17 08:05:15 +0000

    [Raider.IO] Classic Database Refresh

--- db/db_classic_recruitment_us_characters.lua
+++ db/db_classic_recruitment_us_characters.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2025 by RaiderIO, Inc.
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="us",date="2026-07-16T08:04:40Z",numCharacters=4,db={}}
+local provider={name=...,data=3,region="us",date="2026-07-17T08:04:56Z",numCharacters=4,db={}}
 local F
 
 F = function() provider.db["Galakras"]={0,"Escolar","Hirame"} end F()
--- db/db_classic_recruitment_us_lookup.lua
+++ db/db_classic_recruitment_us_lookup.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2025 by RaiderIO, Inc.
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="us",date="2026-07-16T08:04:40Z",numCharacters=4,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
+local provider={name=...,data=3,region="us",date="2026-07-17T08:04:56Z",numCharacters=4,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
 local F
 
 -- chunk size: 8
