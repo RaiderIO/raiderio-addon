1dc9e1f65b5c940d5de9379c931dfb83fa833d53
Jah Raphael <jahraphael@yahoo.com>
2026-08-04 08:42:06 +0000

    [Raider.IO] Database Refresh

--- db/db_recruitment_cn_lookup.lua
+++ db/db_recruitment_cn_lookup.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2026 by RaiderIO, Inc.
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="cn",date="2026-08-03T08:22:01Z",numCharacters=30678,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
+local provider={name=...,data=3,region="cn",date="2026-08-04T08:27:10Z",numCharacters=30890,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
 local F
 
 -- chunk size: 24
--- db/db_score_tiers.lua
+++ db/db_score_tiers.lua
@@ -1,5 +1,5 @@
 --
--- Generated on 2026-08-03T08:45:37Z. DO NOT EDIT.
+-- Generated on 2026-08-04T08:34:18Z. DO NOT EDIT.
 --
 -- Ranges: {"epic":[3426,4375],"superior":[3101,3425],"uncommon":[2376,3100],"common":[200,2375]}
 --
