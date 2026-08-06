27fb3720cf0b9928f530c9eb566b14a0e7012091
Jah Raphael <jahraphael@yahoo.com>
2026-08-06 08:17:22 +0000

    [Raider.IO] Database Refresh

--- db/db_recruitment_cn_characters.lua
+++ db/db_recruitment_cn_characters.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2026 by RaiderIO, Inc.
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="cn",date="2026-08-05T07:56:14Z",numCharacters=31514,db={}}
+local provider={name=...,data=3,region="cn",date="2026-08-06T07:58:35Z",numCharacters=31835,db={}}
 local F
 
 F = function() provider.db["燃烧之刃"]={0,"心月不皈","秦君明月","秦明月"} end F()
--- db/db_recruitment_cn_lookup.lua
+++ db/db_recruitment_cn_lookup.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2026 by RaiderIO, Inc.
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="cn",date="2026-08-05T07:56:14Z",numCharacters=31514,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
+local provider={name=...,data=3,region="cn",date="2026-08-06T07:58:35Z",numCharacters=31835,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
 local F
 
 -- chunk size: 24
