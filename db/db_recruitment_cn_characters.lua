20bff11c72c42bbf22dfbcc672b51605e7928b11
Jah Raphael <jahraphael@yahoo.com>
2026-08-10 08:20:15 +0000

    [Raider.IO] Database Refresh

--- db/db_recruitment_cn_characters.lua
+++ db/db_recruitment_cn_characters.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2026 by RaiderIO, Inc.
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="cn",date="2026-08-09T08:06:57Z",numCharacters=33015,db={}}
+local provider={name=...,data=3,region="cn",date="2026-08-10T07:55:50Z",numCharacters=33475,db={}}
 local F
 
 F = function() provider.db["燃烧之刃"]={0,"心月不皈","秦君明月","秦明月"} end F()
