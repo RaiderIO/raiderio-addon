3c255cc52bb2e53c141c5ce83c20b246e89e2739
Jah Raphael <jahraphael@yahoo.com>
2026-08-27 07:57:15 +0000

    [Raider.IO] Database Refresh

--- db/db_recruitment_cn_characters.lua
+++ db/db_recruitment_cn_characters.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2026 by RaiderIO, Inc.
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="cn",date="2026-08-25T07:53:45Z",numCharacters=39641,db={}}
+local provider={name=...,data=3,region="cn",date="2026-08-27T07:48:35Z",numCharacters=40095,db={}}
 local F
 
 F = function() provider.db["血环"]={0,"狄安娜王妃"} end F()
