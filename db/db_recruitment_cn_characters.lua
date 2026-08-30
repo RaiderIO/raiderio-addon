586171d6a0a64250627c3a71784052b6f387c0c9
Jah Raphael <jahraphael@yahoo.com>
2026-08-30 08:22:32 +0000

    [Raider.IO] Database Refresh

--- db/db_recruitment_cn_characters.lua
+++ db/db_recruitment_cn_characters.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2026 by RaiderIO, Inc.
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="cn",date="2026-08-29T08:01:14Z",numCharacters=39570,db={}}
+local provider={name=...,data=3,region="cn",date="2026-08-30T08:09:36Z",numCharacters=39917,db={}}
 local F
 
 F = function() provider.db["血环"]={0,"狄安娜王妃"} end F()
--- db/db_recruitment_cn_lookup.lua
+++ db/db_recruitment_cn_lookup.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2026 by RaiderIO, Inc.
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="cn",date="2026-08-29T08:01:14Z",numCharacters=39570,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
+local provider={name=...,data=3,region="cn",date="2026-08-30T08:09:36Z",numCharacters=39917,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
 local F
 
 -- chunk size: 10
