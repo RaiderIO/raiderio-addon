f542b2e1660773e5561e902b89952c44452352a9
Jah Raphael <jahraphael@yahoo.com>
2026-09-10 09:43:11 +0000

    [Raider.IO] Database Refresh

--- db/db_recruitment_cn_lookup.lua
+++ db/db_recruitment_cn_lookup.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2026 by RaiderIO, Inc.
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="cn",date="2026-09-08T08:18:48Z",numCharacters=39296,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
+local provider={name=...,data=3,region="cn",date="2026-09-10T09:05:48Z",numCharacters=39181,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
 local F
 
 -- chunk size: 10
